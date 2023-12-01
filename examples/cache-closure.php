1260e71eca3950b0624b186e23a993127cd84cd6
chipslays <gorshkov34@gmail.com>
2023-12-01 13:21:08 +0400

    refactor

--- examples/cache-closure.php
+++ examples/cache-closure.php
@@ -6,16 +6,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $cache = new Cache;
 
-$data = $cache->through(function () {
-    return time();
-});
+$closure = function () {
+    return mt_rand();
+};
 
-dump($data);
-
-sleep(2);
-
-$data = $cache->through(function () {
-    return time();
-});
-
-dump($data);
+echo $cache->through($closure) . PHP_EOL;
+echo $cache->through($closure) . PHP_EOL;
